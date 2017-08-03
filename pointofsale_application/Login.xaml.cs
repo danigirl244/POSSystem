e11b2e63e9d9459313acd6fea971f50422ec8a54
Rob Smith <robert.smith089@gmail.com>
2017-08-02 22:26:49 -0600

    Adjusted Login & Menu classes
    Swicthed them from basic classes to WPF Windows and implemented initial logic for the login class.

---
+++ pointofsale_application/Login.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+
+namespace pointofsale_application
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        Menu menu = new Menu();
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxEmpID.Text.Length == 0 || textBoxEmpID.Text.Length != 6)
+            {
+                errormessage.Text = "Enter employee id.";
+                textBoxEmpID.Focus();
+            }
+            else if (!Regex.IsMatch(textBoxEmpID.Text, @"^[0-9]+$") || textBoxEmpID.Text.Length != 6)
+            {
+                errormessage.Text = "Enter a valid employee id.";
+                textBoxEmpID.Select(0, textBoxEmpID.Text.Length);
+                textBoxEmpID.Focus();
+            }
+            else
+            {
+                string empID = textBoxEmpID.Text;
+
+                SqlConnection con = new SqlConnection("<Specific info required from Database team>");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from Employees where EmpID='" + empID + "'", con);//Not sure if table name or column names are correct
+                cmd.CommandType = CommandType.Text;
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = cmd;
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                if (dataSet.Tables[0].Rows.Count > 0)
+                {
+                    string username = dataSet.Tables[0].Rows[0]["FirstName"].ToString() + " " + dataSet.Tables[0].Rows[0]["LastName"].ToString();//This can be change/removed depending on the data being stored
+                    menu.TextBlockName.Text = username;
+                    menu.Show();
+                    Close();
+                }
+                else
+                {
+                    errormessage.Text = "Invalid Identification.  Enter an existing employee id.";
+                }
+                con.Close();
+            }
+        }
+    }
+}
