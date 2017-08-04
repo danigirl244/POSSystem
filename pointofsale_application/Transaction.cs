2b35efb46a3c2f5fdbf47ac75fdf370e2742e402
Shane <shaneraptor16@gmail.com>
2017-08-03 19:21:37 -0600

    Transaction class, more detail
    Did more stuff in methods

--- pointofsale_application/Transaction.cs
+++ pointofsale_application/Transaction.cs
@@ -33,21 +33,26 @@ namespace pointofsale_application
             ID = userID;
         }
 
-        //Add Item to Cart
+        //Add Item to 'Cart'
         public void addItem()
         {
-           
+           //cartList.add(item)  'item' will be the oject of items. 
         }
 
-        //Delete Item From Cart
+        //Delete Item From 'Cart'
         public void removeItem()
         {
-
+            //cartList.Remove() Removes item from list.
         }
 
         //subtotal
         public double printSubTotal()
         {
+            
+            foreach (object item in cartList)
+            {
+                //subtotal = subtotal + item.price; 'item.price' is a placeholder for whatever the price section is called
+            }
 
             return subtotal;
         }
