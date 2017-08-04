96ff507ff51e21c8df5138e81946da5d20ee5f97
Dani Meyer <ddmeyer@student.neumont.edu>
2017-08-03 19:10:10 -0600

    Added connection to DB

--- pointofsale_application/DatabaseAccess.cs
+++ pointofsale_application/DatabaseAccess.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace pointofsale_application
 {
     class DatabaseAccess
     {
+        void ConnectDB()
+        {
+        }
     }
 }
