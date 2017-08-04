6e3b6b5e8f5a8bf24c392db23a03054b67a952aa
Shane <shaneraptor16@gmail.com>
2017-08-03 18:43:21 -0600

    Starting Transaction Class
    Shelled out the transaction class

--- pointofsale_application/Transaction.cs
+++ pointofsale_application/Transaction.cs
@@ -8,5 +8,60 @@ namespace pointofsale_application
 {
     class Transaction
     {
+        private static double taxPercentage = .0685;
+        private double subtotal;
+        public double SubTotal
+        {
+            get { return subtotal; }
+            set { subtotal = value; }
+        }
+        private double total;
+        public double Total
+        {
+            get { return total; }
+            set { total = value; }
+        }
+        private int ID;
+
+        List<Object> cartList = new List<Object>();
+        
+
+
+
+        public Transaction(int userID /*, string paymentType */)
+        {
+            ID = userID;
+        }
+
+        //Add Item to Cart
+        public void addItem()
+        {
+           
+        }
+
+        //Delete Item From Cart
+        public void removeItem()
+        {
+
+        }
+
+        //subtotal
+        public double printSubTotal()
+        {
+
+            return subtotal;
+        }
+
+        //Final Price with Tax
+        public double printTotal()
+        {
+
+            total = subtotal * taxPercentage;
+
+            return total;
+        }
+        
+
+        
     }
 }
