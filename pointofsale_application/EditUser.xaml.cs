d5e0aa31940fd3416b62598613646eaf4ee974f5
slayerSahara <sjones@student.neumont.edu>
2017-08-23 18:44:08 -0600

    Adding a new product adds it to next row instead of adding to column

--- pointofsale_application/EditUser.xaml.cs
+++ pointofsale_application/EditUser.xaml.cs
@@ -39,9 +39,9 @@ namespace pointofsale_application
         {
 
             int count = 0;
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 5; i++)
             {
-                for (int j = 0; j < 4; j++)
+                for (int j = 0; j < 5; j++)
                 {
                     Button newBtn = new Button();
                     if (Emp.Count > count)
